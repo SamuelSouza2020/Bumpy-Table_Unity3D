8584a4418808240f18f86565667361eb45fe4fa9
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2021-12-31 16:10:43 -0300

    Move Camera
    Camera movendo no inicio da cena

---
+++ LabirintoClone/Assets/Scripts/CamGame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamGame : MonoBehaviour
+{
+    bool ok = false;
+    float vel = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!ok)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0, 15, 0), vel * Time.deltaTime);
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), vel * Time.deltaTime);
+            if(transform.position.y >= 9)
+            {
+                vel = 0.6f;
+                if(transform.position.y >= 11)
+                {
+                    vel = 0.8f;
+                }
+            }
+            if(transform.position.y >= 14.95)
+            {
+                ok = true;
+            }
+        }
+    }
+}
