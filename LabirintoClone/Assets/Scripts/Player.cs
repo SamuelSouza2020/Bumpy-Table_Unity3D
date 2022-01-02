a9186d9b043dc8f90c16e95abb942970a8c9b7a9
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-02 10:34:37 -0300

    Move Player
    Colocado para o player se mover quando a câmera terminar de se posicionar.

--- LabirintoClone/Assets/Scripts/Player.cs
+++ LabirintoClone/Assets/Scripts/Player.cs
@@ -9,25 +9,30 @@ public class Player : MonoBehaviour
     public bool fora = true;
     [SerializeField]
     Vector3 nPos;
+    CamGame gameCam;
 
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        gameCam = GameObject.Find("Main Camera").GetComponent<CamGame>();
     }
     void Update()
     {
-        if (fora)
+        if(gameCam.ok)
         {
-            tiltX = Input.acceleration.x * sensib;
-            tiltY = Input.acceleration.y * sensib;
-            rig.velocity = new Vector3(tiltX, rig.velocity.y, tiltY);
-        }
-        else
-        {
-            rig.velocity = new Vector3(0, 0, 0);
-            transform.position = Vector3.Lerp(transform.position, new Vector3(nPos.x, transform.position.y, nPos.z), 3 * Time.deltaTime);
-            gameObject.GetComponent<Collider>().enabled = false;
-            rig.velocity = new Vector3(0, -1, 0);
+            if (fora)
+            {
+                tiltX = Input.acceleration.x * sensib;
+                tiltY = Input.acceleration.y * sensib;
+                rig.velocity = new Vector3(tiltX, rig.velocity.y, tiltY);
+            }
+            else
+            {
+                rig.velocity = new Vector3(0, 0, 0);
+                transform.position = Vector3.Lerp(transform.position, new Vector3(nPos.x, transform.position.y, nPos.z), 3 * Time.deltaTime);
+                gameObject.GetComponent<Collider>().enabled = false;
+                rig.velocity = new Vector3(0, -1, 0);
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
