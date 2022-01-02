6118330b834f3e31e30e1fa0605b1be1333e3c1e
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-02 10:59:00 -0300

    Temporizador
    Tempo para iniciar a fase

--- LabirintoClone/Assets/Scripts/CamGame.cs
+++ LabirintoClone/Assets/Scripts/CamGame.cs
@@ -1,16 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CamGame : MonoBehaviour
 {
     //BallD vai utilizar o ok para verificar se a camera já se posicionou
     public bool ok = false;
     float vel = 0.3f;
+    float tempoSt = 5.5f, tempGO = 1.2f;
+
+    GameObject canvasTP;
+    Text tTempSt;
+
+    private void Start()
+    {
+        tTempSt = GameObject.Find("TempoStart").GetComponent<Text>();
+        canvasTP = GameObject.Find("CanvasTp");
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if(tempGO > 0)
+        {
+            if (tempoSt > 0)
+            {
+                tempoSt -= Time.deltaTime;
+                tTempSt.text = tempoSt.ToString("0");
+            }
+            else
+            {
+                tTempSt.text = "GO";
+                tempGO -= Time.deltaTime;
+                if (tempGO < 0.2)
+                {
+                    canvasTP.SetActive(false);
+                }
+            }
+        }
         if(!ok)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(0, 15, 0), vel * Time.deltaTime);
