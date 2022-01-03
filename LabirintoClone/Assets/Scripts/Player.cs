de88ca7e5bab31defe3e02e97e8eac601894dcd9
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-03 11:32:00 -0300

    Atualização do Som
    Mudança de volume.
    Tag Parede add
    Som de batendo na parede.

--- LabirintoClone/Assets/Scripts/Player.cs
+++ LabirintoClone/Assets/Scripts/Player.cs
@@ -16,7 +16,8 @@ public class Player : MonoBehaviour
     GameObject canvasTP;
     Text tTempSt;
 
-    AudioSource aud;
+    [SerializeField]
+    AudioSource aud, aud2;
 
     void Start()
     {
@@ -25,19 +26,29 @@ public class Player : MonoBehaviour
         tTempSt = GameObject.Find("TempoStart").GetComponent<Text>();
         canvasTP = GameObject.Find("CanvasTp");
 
-        aud = GetComponent<AudioSource>();
+        //aud = GetComponent<AudioSource>();
+        //aud2 = GetComponent<AudioSource>();
         mov = false;
     }
     void Update()
     {
+        Debug.Log(rig.velocity.magnitude);
         if(gameCam.ok)
         {
             if (fora)
             {
-                if(rig.velocity.magnitude > 0.7)
-                    aud.volume = 0.7f;
+                if(rig.velocity.magnitude > 3)
+                    aud.volume = 0.5f;
+                else if(rig.velocity.magnitude > 2 && rig.velocity.magnitude <= 3)
+                    aud.volume = 0.4f;
+                else if (rig.velocity.magnitude > 1 && rig.velocity.magnitude <= 2)
+                    aud.volume = 0.3f;
+                else if (rig.velocity.magnitude > 0.7 && rig.velocity.magnitude <= 0.1)
+                    aud.volume = 0.2f;
+                else if (rig.velocity.magnitude > 0.3 && rig.velocity.magnitude <= 0.7)
+                    aud.volume = 0.13f;
                 else
-                    aud.volume = rig.velocity.magnitude;
+                    aud.volume = 0.08f;
 
 
                 if (rig.velocity.magnitude >= 0.2 && !mov)
@@ -84,6 +95,14 @@ public class Player : MonoBehaviour
             canvasTP.SetActive(true);
             tTempSt.text = "PERDEU";
             fora = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Parede"))
+        {
+            aud2.Play();
         }
     }
     IEnumerator PerdeuGm()
