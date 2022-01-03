94ebe55c1fed1a6e9f40baf08e1dad7e9e24f5c7
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-03 09:54:56 -0300

    OndeEstou?
    Script "OndeEstou" é usado para identificar em qual cenário o player está

---
+++ LabirintoClone/Assets/Scripts/OndeEstou.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OndeEstou : MonoBehaviour
+{
+    public static OndeEstou instance;
+
+    public int fase = -1;
+    public string faseN;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        SceneManager.sceneLoaded += VerificaFase;
+    }
+    void VerificaFase(Scene cena, LoadSceneMode modo)
+    {
+        fase = SceneManager.GetActiveScene().buildIndex;
+        faseN = SceneManager.GetActiveScene().name;
+    }
+}
