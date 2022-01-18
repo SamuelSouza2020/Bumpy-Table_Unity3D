8452fbc96eb59f2fd3d01e7a71115386c1850106
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-18 16:34:10 -0300

    MoveObj Comentado
    Método simples para movimentar um objeto em zigue-zague

--- LabirintoClone/Assets/Scripts/MoveObj.cs
+++ LabirintoClone/Assets/Scripts/MoveObj.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MoveObj : MonoBehaviour
 {
+    //Todos os comandos são encontrados no Unity Documentation
+    /*Script utilizado para movimentar a parede na fase 2
+    Método simples para movimentar um objeto em zigue-zague
+    Existe varios métodos para fazer essa função, como animação e motores
+    */
+    
+    //variavel (lado) utilizada para não dar conflito no IF
     int lado = 0;
 
     private void Update()
     {
+        //Movimento por limite. Caso ele chegue no limite proposto no IF ele vai para o outro lado.
         if (transform.position.z <= 3.7 && lado == 0)
         {
             transform.Translate(new Vector3(0, 0, 4 * Time.deltaTime));
