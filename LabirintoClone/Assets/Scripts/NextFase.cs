4ffaed533d59a35d399312c017b312aa8f939e13
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-18 16:04:59 -0300

    NextPhase Comentado
    Exemplo de outro comando que pode ser utilizado, para facilitar.

--- LabirintoClone/Assets/Scripts/NextFase.cs
+++ LabirintoClone/Assets/Scripts/NextFase.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class NextFase : MonoBehaviour
 {
+    //Todos os comandos são encontrados no Unity Documentation
+    /*
+    proFase (int) utilizado para saber o número da próxima fase, em caso de muitas fases criar um nome script GameManager com Singleton.
+    Nele será armazenado o número da fase e pode utilizar incremento para avançar de fase (para não fazer tudo manualmente)
+    Ou utilizando o script OndeEstou, Exemplo:
+    SceneManager.LoadScene(OndeEstou.instance.fase++);
+    */
     [SerializeField]
     int proFase = 2;
     private void OnTriggerEnter(Collider other)
